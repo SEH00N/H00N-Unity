1b7b772407ba1c2a260e490597d7e9ea10f6878a
SEH00N <06psh0501@gmail.com>
2025-08-18 23:45:18 +0900

    [Resources] Addressable Asset Fix

--- H00N-Unity/Assets/H00N/Resources.Addressables/Runtime/AddressableAsset.cs
+++ H00N-Unity/Assets/H00N/Resources.Addressables/Runtime/AddressableAsset.cs
@@ -7,48 +7,30 @@ namespace H00N.Resources.Addressables
     [System.Serializable]
     public class AddressableAsset<T> where T : Object
     {
-        private enum EState
-        {
-            Uninitialized,
-            Initializing,
-            Initialized
-        }
-
         [SerializeField] string key = null;
         public string Key => key;
 
         private T asset = null;
         public T Asset => asset;
 
-        private EState state = EState.Uninitialized;
-        public bool Initialized => state == EState.Initialized;
-
-        public AddressableAsset()
-        {
-            state = EState.Uninitialized;
-        }
-
         public async UniTask InitializeAsync()
         {
-            if(Initialized)
-                return;
-
-            if(state == EState.Initializing)
-            {
-                await UniTask.WaitUntil(() => Initialized);
-                return;
-            }
-
-            state = EState.Initializing;
             if (string.IsNullOrEmpty(key))
             {
                 Debug.LogError("Key is null or empty.");
-                state = EState.Initialized;
                 return;
             }
 
             asset = await ResourceManager.LoadResourceAsync<T>(key);
-            state = EState.Initialized;
+        }
+
+        public void Release()
+        {
+            if (string.IsNullOrEmpty(key))
+                return;
+
+            ResourceManager.ReleaseResource(key);
+            asset = null;
         }
 
         public static implicit operator T(AddressableAsset<T> reference) => reference.Asset;
